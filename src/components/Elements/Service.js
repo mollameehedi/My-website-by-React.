98c322b932a7a61c9190900d5c1e32fe0498c40e
mollameehedi <mollameehedi@gmail.com>
2022-06-30 07:03:48 +0600

    Service part complete

--- src/components/Elements/Service.js
+++ src/components/Elements/Service.js
@@ -1,8 +1,79 @@
 import React from 'react'
+import {Container,Row, Col} from 'react-bootstrap';
+import {FaLaptopCode,FaArrowRight} from 'react-icons/fa';
 
 const Service = () => {
   return (
-    <div>Service</div>
+    <Container id='HomeService'>
+          <Row>
+              <Col md={12}>
+                <div className='ServiceHeader'>
+                    <h5>FEATURES</h5>
+                    <h2>What I Do</h2>
+                </div>
+              </Col>
+              <Col md={4}>
+                <div className='serviceItem  ms-3 me-3'>
+                    <div className='serviceItemContent'>
+                      <FaLaptopCode className='serviceIcon'/>
+                        <h3>Web Design</h3>
+                        <p>I throw myself down among the tall grass by the stream as I lie close to the earth. I throw myself down among the tall grass by the stream as I lie close to the earth.</p>
+                        <FaArrowRight className='arrow'/>
+                    </div>
+                </div>
+              </Col>
+              <Col md={4}>
+                <div className='serviceItem  ms-3 me-3'>
+                    <div className='serviceItemContent'>
+                      <FaLaptopCode className='serviceIcon'/>
+                        <h3>Web Design</h3>
+                        <p>I throw myself down among the tall grass by the stream as I lie close to the earth. I throw myself down among the tall grass by the stream as I lie close to the earth.</p>
+                        <FaArrowRight className='arrow'/>
+                    </div>
+                </div>
+              </Col>
+              <Col md={4}>
+                <div className='serviceItem ms-3 me-3'>
+                    <div className='serviceItemContent'>
+                      <FaLaptopCode className='serviceIcon'/>
+                        <h3>Web Design</h3>
+                        <p>I throw myself down among the tall grass by the stream as I lie close to the earth. I throw myself down among the tall grass by the stream as I lie close to the earth.</p>
+                        <FaArrowRight className='arrow'/>
+                    </div>
+                </div>
+              </Col>
+              <Col md={4}>
+                <div className='serviceItem  ms-3 me-3'>
+                    <div className='serviceItemContent'>
+                      <FaLaptopCode className='serviceIcon'/>
+                        <h3>Web Design</h3>
+                        <p>I throw myself down among the tall grass by the stream as I lie close to the earth. I throw myself down among the tall grass by the stream as I lie close to the earth.</p>
+                        <FaArrowRight className='arrow'/>
+                    </div>
+                </div>
+              </Col>
+              <Col md={4}>
+                <div className='serviceItem  ms-3 me-3'>
+                    <div className='serviceItemContent'>
+                      <FaLaptopCode className='serviceIcon'/>
+                        <h3>Web Design</h3>
+                        <p>I throw myself down among the tall grass by the stream as I lie close to the earth. I throw myself down among the tall grass by the stream as I lie close to the earth.</p>
+                        <FaArrowRight className='arrow'/>
+                    </div>
+                </div>
+              </Col>
+              <Col md={4}>
+                <div className='serviceItem  ms-3 me-3'>
+                    <div className='serviceItemContent'>
+                      <FaLaptopCode className='serviceIcon'/>
+                        <h3>Web Design</h3>
+                        <p>I throw myself down among the tall grass by the stream as I lie close to the earth. I throw myself down among the tall grass by the stream as I lie close to the earth.</p>
+                        <FaArrowRight className='arrow'/>
+                    </div>
+                </div>
+              </Col>
+          </Row>
+    </Container>
   )
 }
 
